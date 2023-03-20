9e01177ff7dbb13c82e70bb6ff3c8e828fdb4f8b
BanditHeelerPDX <kirk.alexander.steele@gmail.com>
2023-03-20 14:23:26 -0700

    bringing it together and filling the gaps

--- script/script.js
+++ script/script.js
@@ -1,129 +1,133 @@
-const apiKey = '796d4a60796f715c7b650eb7c5eade88';
-let userInput = document.getElementById('search');
-const submit = document.getElementById('searchBtn');
-const persistentLocationsEl = document.getElementById('persistentLocations');
+const apiKey = "796d4a60796f715c7b650eb7c5eade88";
+let userInput = document.getElementById("search");
+const submit = document.getElementById("searchBtn");
+const locationEl = document.getElementById("location");
+const persistentLocationsEl = document.getElementById("persistentLocations");
 let persistentLocations = [];
 
-submit.addEventListener('click', function(event) {
+submit.addEventListener("click", function (event) {
     event.preventDefault();
+    doTheDamnThang();
+  });
 
+  function doTheDamnThang() {
     const location = userInput.value;
-    
+
     const coordURL = `https://api.openweathermap.org/geo/1.0/direct?q=${location}&appid=${apiKey}`;
 
     fetch(coordURL)
-        .then((response) => response.json())
-        .then((geoData) => {
-            console.log(geoData);
-            const lat = geoData[0].lat;
-            const lon = geoData[0].lon;
-    
-    const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
-
-    fetch(weatherURL)
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-    const mainTemp = parseInt(data.main.temp);
-    const feelsLikeTemp = parseInt(data.main.feels_like);
-    const minTemp = parseInt(data.main.temp_min);
-    const maxTemp = parseInt(data.main.temp_max);
-    const cloudiness = data.clouds.all;
-    const snowVolumeLast3Hrs = data.snow ? data.snow['3h'] : 'N/A';
-
-    const mainTempEl = document.createElement("div");
-    const feelsLikeTempEl = document.createElement("p");
-    const minTempEl = document.createElement("p");
-    const maxTempEl = document.createElement("p");
-    const cloudinessEl = document.createElement("p");
-    const snowVolumeLast3HrsEl = document.createElement("p");
-
-    mainTempEl.textContent = `Main Temp: ${mainTemp}F`;
-    feelsLikeTempEl.textContent = `Feels Like Temp: ${feelsLikeTemp}F`;
-    minTempEl.textContent = `Min Temp: ${minTemp}F`;
-    maxTempEl.textContent = `Max Temp: ${maxTemp}F`;
-    cloudinessEl.textContent = `Cloudiness: ${cloudiness}%`;
-    snowVolumeLast3HrsEl.textContent = `Snow Volume (last 3 hours): ${snowVolumeLast3Hrs}`;
-
-    const containerEl = document.getElementById("currentWeatherEl");
-    containerEl.innerHTML = '';
-    containerEl.appendChild(mainTempEl);
-    containerEl.appendChild(feelsLikeTempEl);
-    containerEl.appendChild(minTempEl);
-    containerEl.appendChild(maxTempEl);
-    containerEl.appendChild(cloudinessEl);
-    containerEl.appendChild(snowVolumeLast3HrsEl);
-
-    persistentLocations.push(location);
-    populateLocations(persistentLocations);
-  })
-  .catch(error => {
-    console.error(error);
-    
-  });
-
-  const wizardURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
-
-    fetch(wizardURL)
       .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        const forecast = data.list;
+      .then((geoData) => {
+        console.log(geoData);
+        const lat = geoData[0].lat;
+        const lon = geoData[0].lon;
 
-        const forecastEl = document.getElementById("fiveDayForecast");
-        forecastEl.innerHTML = '';
+        const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
 
-        for (let i = 0; i < forecast.length; i += 8) {
-          const forecastData = forecast[i];
+        fetch(weatherURL)
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            const name = data.name;
+            const mainTemp = parseInt(data.main.temp);
+            const feelsLikeTemp = parseInt(data.main.feels_like);
+            const minTemp = parseInt(data.main.temp_min);
+            const maxTemp = parseInt(data.main.temp_max);
+            const cloudiness = data.clouds.all;
+            const snowVolumeLast3Hrs = data.snow ? data.snow["3h"] : "N/A";
 
-          const date = new Date(forecastData.dt * 1000);
-          const dayOfWeek = date.toLocaleDateString('en-US', { weekday: 'long' });
+            const mainTempEl = document.createElement("div");
+            const feelsLikeTempEl = document.createElement("p");
+            const minTempEl = document.createElement("p");
+            const maxTempEl = document.createElement("p");
+            const cloudinessEl = document.createElement("p");
+            const snowVolumeLast3HrsEl = document.createElement("p");
 
-          const minTemp = parseInt(forecastData.main.temp_min);
-          const maxTemp = parseInt(forecastData.main.temp_max);
-          const chanceOfPrecip = forecastData.pop * 100;
-          const cloudiness = forecastData.clouds.all;
+            locationEl.textContent = `The weather in ${name}:`
+            mainTempEl.textContent = `Main Temp: ${mainTemp}F`;
+            feelsLikeTempEl.textContent = `Feels Like Temp: ${feelsLikeTemp}F`;
+            minTempEl.textContent = `Min Temp: ${minTemp}F`;
+            maxTempEl.textContent = `Max Temp: ${maxTemp}F`;
+            cloudinessEl.textContent = `Cloudiness: ${cloudiness}%`;
+            snowVolumeLast3HrsEl.textContent = `Snow Volume (last 3 hours): ${snowVolumeLast3Hrs}`;
 
-          const forecastCard = document.createElement("div");
-          forecastCard.classList.add("card");
+            const containerEl = document.getElementById("currentWeatherEl");
+            containerEl.innerHTML = "";
+            containerEl.appendChild(mainTempEl);
+            containerEl.appendChild(feelsLikeTempEl);
+            containerEl.appendChild(minTempEl);
+            containerEl.appendChild(maxTempEl);
+            containerEl.appendChild(cloudinessEl);
+            containerEl.appendChild(snowVolumeLast3HrsEl);
 
-          const cardBody = document.createElement("div");
-          cardBody.classList.add("card-body");
+            persistentLocations.push(location);
+            populateLocations(persistentLocations);
+          })
+          .catch((error) => {
+            console.error(error);
+          });
 
-          const dayOfWeekEl = document.createElement("h5");
-          dayOfWeekEl.textContent = dayOfWeek;
+        const wizardURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
 
-          const minTempEl = document.createElement("p");
-          minTempEl.textContent = `Min Temp: ${minTemp}F`;
+        fetch(wizardURL)
+          .then((response) => response.json())
+          .then((data) => {
+            console.log(data);
+            const forecast = data.list;
 
-          const maxTempEl = document.createElement("p");
-          maxTempEl.textContent = `Max Temp: ${maxTemp}F`;
+            const forecastEl = document.getElementById("fiveDayForecast");
+            forecastEl.innerHTML = "";
 
-          const chanceOfPrecipEl = document.createElement("p");
-          chanceOfPrecipEl.textContent = `Chance of Precipitation: ${chanceOfPrecip}%`;
+            for (let i = 0; i < forecast.length; i += 8) {
+              const forecastData = forecast[i];
 
-          const cloudinessEl = document.createElement("p");
-          cloudinessEl.textContent = `Cloudiness: ${cloudiness}%`;
+              const date = new Date(forecastData.dt * 1000);
+              const dayOfWeek = date.toLocaleDateString("en-US", {
+                weekday: "long",
+              });
 
-          cardBody.appendChild(dayOfWeekEl);
-          cardBody.appendChild(minTempEl);
-          cardBody.appendChild(maxTempEl);
-          cardBody.appendChild(chanceOfPrecipEl);
-          cardBody.appendChild(cloudinessEl);
+              const minTemp = parseInt(forecastData.main.temp_min);
+              const maxTemp = parseInt(forecastData.main.temp_max);
+              const chanceOfPrecip = parseInt(forecastData.pop * 100);
+              const cloudiness = forecastData.clouds.all;
 
-          forecastCard.appendChild(cardBody);
-          forecastEl.appendChild(forecastCard);
-        }
-      })
-      .catch((error) => {
-        console.error(error);
+              const forecastCard = document.createElement("div");
+              forecastCard.classList.add("card");
+
+              const cardBody = document.createElement("div");
+              cardBody.classList.add("card-body");
+
+              const dayOfWeekEl = document.createElement("h4");
+              dayOfWeekEl.textContent = dayOfWeek;
+
+              const minTempEl = document.createElement("p");
+              minTempEl.textContent = `Min Temp: ${minTemp}F`;
+
+              const maxTempEl = document.createElement("p");
+              maxTempEl.textContent = `Max Temp: ${maxTemp}F`;
+
+              const chanceOfPrecipEl = document.createElement("p");
+              chanceOfPrecipEl.textContent = `Chance of Precipitation: ${chanceOfPrecip}%`;
+
+              const cloudinessEl = document.createElement("p");
+              cloudinessEl.textContent = `Cloudiness: ${cloudiness}%`;
+
+              cardBody.appendChild(dayOfWeekEl);
+              cardBody.appendChild(minTempEl);
+              cardBody.appendChild(maxTempEl);
+              cardBody.appendChild(chanceOfPrecipEl);
+              cardBody.appendChild(cloudinessEl);
+
+              forecastCard.appendChild(cardBody);
+              forecastEl.appendChild(forecastCard);
+            }
+          })
+          .catch((error) => {
+            console.error(error);
+          });
       });
-  })
-})
-.catch(error => {
-    console.error(error);
-})
-
+  }
+  
 // function populateLocations(locations) {
 //     persistentLocationsEl.innerHTML = '';
 //     const beenThere = document.createElement('ul');
@@ -140,21 +144,21 @@ submit.addEventListener('click', function(event) {
 // }
 
 function populateLocations(locations) {
-    persistentLocationsEl.innerHTML = '';
-    const beenThere = document.createElement('ul');
-    for (let location of locations) {
-      if (!document.querySelector(`[data-location="${location}"]`)) {
-        const visited = document.createElement('li');
-        const link = document.createElement('a');
-        link.setAttribute('href', '#');
-        link.setAttribute('data-location', location);
-        link.textContent = location;
-        visited.appendChild(link);
-        beenThere.appendChild(visited);
-        link.addEventListener('click', () => {
-          fetchWeatherData(location);
-        });
-      }
+  persistentLocationsEl.innerHTML = "";
+  const beenThere = document.createElement("ul");
+  for (let location of locations) {
+    if (!document.querySelector(`[data-location="${location}"]`)) {
+      const visited = document.createElement("li");
+      const link = document.createElement("a");
+      link.setAttribute("href", "#");
+      link.setAttribute("data-location", location);
+      link.textContent = location;
+      visited.appendChild(link);
+      beenThere.appendChild(visited);
+      link.addEventListener("click", () => {
+        doTheDamnThang(location);
+      });
     }
-    persistentLocationsEl.appendChild(beenThere);
   }
+  persistentLocationsEl.appendChild(beenThere);
+}
